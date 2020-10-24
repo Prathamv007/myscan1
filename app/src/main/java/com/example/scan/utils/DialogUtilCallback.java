30bc742f547ce56ad33144bd94bee608c61adbf7
Pratham <you@example.com>
2020-10-24 16:52:16 +0530

    quali

---
+++ app/src/main/java/com/example/scan/utils/DialogUtilCallback.java
@@ -0,0 +1,8 @@
+package com.example.scan.utils;
+
+import android.content.DialogInterface;
+
+public interface DialogUtilCallback {
+
+    public void onSave( String textValue, String category );
+}
---
+++ scanlibrary/src/main/obj/local/armeabi-v7a/objs/Scanner/scan.o.d
@@ -0,0 +1,395 @@
+D:/myscan1-master/scanlibrary/src/main/obj/local/armeabi-v7a/objs/Scanner/scan.o: \
+  D:\myscan1-master\scanlibrary\src\main\jni\scan.cpp \
+  D:\myscan1-master\scanlibrary\src\main\jni\com_scanlibrary_ScanActivity.h \
+  sdk\native\jni\include\opencv2\opencv.hpp \
+  sdk\native\jni\include\opencv2\core.hpp \
+  sdk\native\jni\include\opencv2\core\cvdef.h \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\limits.h \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\__config \
+  sdk\native\jni\include\opencv2\core\hal\interface.h \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\cstddef \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\version \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\cstdint \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\stdint.h \
+  sdk\native\jni\include\opencv2\core\version.hpp \
+  sdk\native\jni\include\opencv2\core\base.hpp \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\climits \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\algorithm \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\initializer_list \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\type_traits \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\cstring \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\string.h \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\utility \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\__tuple \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\__debug \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\iosfwd \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\wchar.h \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\memory \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\typeinfo \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\exception \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\cstdlib \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\stdlib.h \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\new \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\limits \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\iterator \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\__functional_base \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\tuple \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\stdexcept \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\cassert \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\atomic \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\functional \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\bit \
+  sdk\native\jni\include\opencv2\core\cvstd.hpp \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\cctype \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\ctype.h \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\string \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\string_view \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\__string \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\cstdio \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\stdio.h \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\cwchar \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\cwctype \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\wctype.h \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\cmath \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\math.h \
+  sdk\native\jni\include\opencv2\core\ptr.inl.hpp \
+  sdk\native\jni\include\opencv2\core\neon_utils.hpp \
+  sdk\native\jni\include\opencv2\core\traits.hpp \
+  sdk\native\jni\include\opencv2\core\matx.hpp \
+  sdk\native\jni\include\opencv2\core\saturate.hpp \
+  sdk\native\jni\include\opencv2\core\fast_math.hpp \
+  sdk\native\jni\include\opencv2\core\types.hpp \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\cfloat \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\float.h \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\vector \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\__bit_reference \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\__split_buffer \
+  sdk\native\jni\include\opencv2\core\mat.hpp \
+  sdk\native\jni\include\opencv2\core\bufferpool.hpp \
+  sdk\native\jni\include\opencv2\core\mat.inl.hpp \
+  sdk\native\jni\include\opencv2\core\persistence.hpp \
+  sdk\native\jni\include\opencv2\core\operations.hpp \
+  sdk\native\jni\include\opencv2\core\cvstd.inl.hpp \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\complex \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\sstream \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\ostream \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\ios \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\__locale \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\mutex \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\__mutex_base \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\chrono \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\ctime \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\ratio \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\system_error \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\__errc \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\cerrno \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\errno.h \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\__threading_support \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\locale.h \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\support\android\locale_bionic.h \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\support\xlocale\__posix_l_fallback.h \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\streambuf \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\locale \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\cstdarg \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\__bsd_locale_fallbacks.h \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\bitset \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\istream \
+  sdk\native\jni\include\opencv2\core\utility.hpp \
+  sdk\native\jni\include\opencv2\core\core_c.h \
+  sdk\native\jni\include\opencv2\core\types_c.h \
+  sdk\native\jni\include\opencv2\core\optim.hpp \
+  sdk\native\jni\include\opencv2\imgproc.hpp \
+  sdk\native\jni\include\opencv2\imgproc\imgproc_c.h \
+  sdk\native\jni\include\opencv2\imgproc\types_c.h \
+  sdk\native\jni\include\opencv2\photo.hpp \
+  sdk\native\jni\include\opencv2\photo\photo_c.h \
+  sdk\native\jni\include\opencv2\video.hpp \
+  sdk\native\jni\include\opencv2\video\tracking.hpp \
+  sdk\native\jni\include\opencv2\video\background_segm.hpp \
+  sdk\native\jni\include\opencv2\video\tracking_c.h \
+  sdk\native\jni\include\opencv2\features2d.hpp \
+  sdk\native\jni\include\opencv2\flann\miniflann.hpp \
+  sdk\native\jni\include\opencv2\flann\defines.h \
+  sdk\native\jni\include\opencv2\flann\config.h \
+  sdk\native\jni\include\opencv2\objdetect.hpp \
+  sdk\native\jni\include\opencv2\objdetect\detection_based_tracker.hpp \
+  sdk\native\jni\include\opencv2\objdetect\objdetect_c.h \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\deque \
+  sdk\native\jni\include\opencv2\calib3d.hpp \
+  sdk\native\jni\include\opencv2\core\affine.hpp \
+  sdk\native\jni\include\opencv2\calib3d\calib3d_c.h \
+  sdk\native\jni\include\opencv2\imgcodecs.hpp \
+  sdk\native\jni\include\opencv2\videoio.hpp \
+  sdk\native\jni\include\opencv2\highgui.hpp \
+  sdk\native\jni\include\opencv2\highgui\highgui_c.h \
+  sdk\native\jni\include\opencv2\imgcodecs\imgcodecs_c.h \
+  sdk\native\jni\include\opencv2\videoio\videoio_c.h \
+  sdk\native\jni\include\opencv2\ml.hpp \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\map \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\__tree \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\__node_handle \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\optional \
+  C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\iostream \
+  sdk\native\jni\include\opencv2\core\core.hpp
+
+D:\myscan1-master\scanlibrary\src\main\jni\com_scanlibrary_ScanActivity.h:
+
+sdk\native\jni\include\opencv2\opencv.hpp:
+
+sdk\native\jni\include\opencv2\core.hpp:
+
+sdk\native\jni\include\opencv2\core\cvdef.h:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\limits.h:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\__config:
+
+sdk\native\jni\include\opencv2\core\hal\interface.h:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\cstddef:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\version:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\cstdint:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\stdint.h:
+
+sdk\native\jni\include\opencv2\core\version.hpp:
+
+sdk\native\jni\include\opencv2\core\base.hpp:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\climits:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\algorithm:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\initializer_list:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\type_traits:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\cstring:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\string.h:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\utility:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\__tuple:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\__debug:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\iosfwd:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\wchar.h:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\memory:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\typeinfo:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\exception:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\cstdlib:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\stdlib.h:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\new:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\limits:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\iterator:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\__functional_base:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\tuple:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\stdexcept:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\cassert:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\atomic:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\functional:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\bit:
+
+sdk\native\jni\include\opencv2\core\cvstd.hpp:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\cctype:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\ctype.h:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\string:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\string_view:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\__string:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\cstdio:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\stdio.h:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\cwchar:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\cwctype:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\wctype.h:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\cmath:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\math.h:
+
+sdk\native\jni\include\opencv2\core\ptr.inl.hpp:
+
+sdk\native\jni\include\opencv2\core\neon_utils.hpp:
+
+sdk\native\jni\include\opencv2\core\traits.hpp:
+
+sdk\native\jni\include\opencv2\core\matx.hpp:
+
+sdk\native\jni\include\opencv2\core\saturate.hpp:
+
+sdk\native\jni\include\opencv2\core\fast_math.hpp:
+
+sdk\native\jni\include\opencv2\core\types.hpp:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\cfloat:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\float.h:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\vector:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\__bit_reference:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\__split_buffer:
+
+sdk\native\jni\include\opencv2\core\mat.hpp:
+
+sdk\native\jni\include\opencv2\core\bufferpool.hpp:
+
+sdk\native\jni\include\opencv2\core\mat.inl.hpp:
+
+sdk\native\jni\include\opencv2\core\persistence.hpp:
+
+sdk\native\jni\include\opencv2\core\operations.hpp:
+
+sdk\native\jni\include\opencv2\core\cvstd.inl.hpp:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\complex:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\sstream:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\ostream:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\ios:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\__locale:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\mutex:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\__mutex_base:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\chrono:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\ctime:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\ratio:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\system_error:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\__errc:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\cerrno:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\errno.h:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\__threading_support:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\locale.h:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\support\android\locale_bionic.h:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\support\xlocale\__posix_l_fallback.h:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\streambuf:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\locale:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\cstdarg:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\__bsd_locale_fallbacks.h:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\bitset:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\istream:
+
+sdk\native\jni\include\opencv2\core\utility.hpp:
+
+sdk\native\jni\include\opencv2\core\core_c.h:
+
+sdk\native\jni\include\opencv2\core\types_c.h:
+
+sdk\native\jni\include\opencv2\core\optim.hpp:
+
+sdk\native\jni\include\opencv2\imgproc.hpp:
+
+sdk\native\jni\include\opencv2\imgproc\imgproc_c.h:
+
+sdk\native\jni\include\opencv2\imgproc\types_c.h:
+
+sdk\native\jni\include\opencv2\photo.hpp:
+
+sdk\native\jni\include\opencv2\photo\photo_c.h:
+
+sdk\native\jni\include\opencv2\video.hpp:
+
+sdk\native\jni\include\opencv2\video\tracking.hpp:
+
+sdk\native\jni\include\opencv2\video\background_segm.hpp:
+
+sdk\native\jni\include\opencv2\video\tracking_c.h:
+
+sdk\native\jni\include\opencv2\features2d.hpp:
+
+sdk\native\jni\include\opencv2\flann\miniflann.hpp:
+
+sdk\native\jni\include\opencv2\flann\defines.h:
+
+sdk\native\jni\include\opencv2\flann\config.h:
+
+sdk\native\jni\include\opencv2\objdetect.hpp:
+
+sdk\native\jni\include\opencv2\objdetect\detection_based_tracker.hpp:
+
+sdk\native\jni\include\opencv2\objdetect\objdetect_c.h:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\deque:
+
+sdk\native\jni\include\opencv2\calib3d.hpp:
+
+sdk\native\jni\include\opencv2\core\affine.hpp:
+
+sdk\native\jni\include\opencv2\calib3d\calib3d_c.h:
+
+sdk\native\jni\include\opencv2\imgcodecs.hpp:
+
+sdk\native\jni\include\opencv2\videoio.hpp:
+
+sdk\native\jni\include\opencv2\highgui.hpp:
+
+sdk\native\jni\include\opencv2\highgui\highgui_c.h:
+
+sdk\native\jni\include\opencv2\imgcodecs\imgcodecs_c.h:
+
+sdk\native\jni\include\opencv2\videoio\videoio_c.h:
+
+sdk\native\jni\include\opencv2\ml.hpp:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\map:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\__tree:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\__node_handle:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\optional:
+
+C:\android-ndk-r21d\build\\..\sources\cxx-stl\llvm-libc++\include\iostream:
+
+sdk\native\jni\include\opencv2\core\core.hpp:
