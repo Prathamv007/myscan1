8a157d451bd3358a5ef20322f80fa16fce846cf8
Pratham <you@example.com>
2020-12-19 13:05:09 +0530

    ocr bro

---
+++ app/src/main/java/com/example/scan/ocr/adapter/PageAdapter.java
@@ -0,0 +1,38 @@
+package com.example.scan.ocr.adapter;
+
+
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.scan.ocr.fragments.MainLayout;
+import com.example.scan.ocr.fragments.ResultLayout;
+
+
+public class PageAdapter extends FragmentPagerAdapter {
+
+    private int numOfTabs;
+
+    public PageAdapter(FragmentManager fm, int numOfTabs) {
+        super(fm);
+        this.numOfTabs = numOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        switch (i) {
+            case 0:
+                return new MainLayout();
+            case 1:
+                return new ResultLayout();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return numOfTabs;
+    }
+}
