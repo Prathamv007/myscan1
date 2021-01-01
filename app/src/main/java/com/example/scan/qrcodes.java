40bb8351c451d3a1a79fcbcd25cbbfdbcd71cb51
Pratham <you@example.com>
2021-01-02 01:29:15 +0530

    qr

---
+++ app/src/main/java/com/example/scan/qrcodes.java
@@ -0,0 +1,76 @@
+package com.example.scan;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+import com.otaliastudios.cameraview.CameraView;
+import com.otaliastudios.cameraview.frame.Frame;
+import com.otaliastudios.cameraview.frame.FrameProcessor;
+
+public class qrcodes extends AppCompatActivity {
+    CameraView camera_view;
+    boolean isDetected=false;
+    Button btn_start_again;
+
+    FirebaseVisionBarcodeDetectorOptions options;
+    FirebaseVisionBarcodeDetector detector;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrcodes);
+        Dexter.withActivity(this).withPermission(Manifest.permission.CAMERA)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+setupCamera();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        Toast.makeText(qrcodes.this,"must accept permission",Toast.LENGTH_SHORT);
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+
+                    }
+                }).check();
+
+    }
+
+    private void setupCamera() {
+        btn_start_again=(Button)findViewById(R.id.btn_again);
+        btn_start_again.setEnabled(isDetected);
+        btn_start_again.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isDetected=!isDetected;
+            }
+        });
+        camera_view=(CameraView)findViewById(R.id.cameraView);
+        camera_view.setLifecycleOwner(this);
+        camera_view.addFrameProcessor(new FrameProcessor() {
+            @Override
+            public void process(@NonNull Frame frame) {
+
+            }
+        });
+    }
+}
